26c5a160e89adc5381b9763eea4a49832cfbbf40
Kartik Raiyani <93818535+kartikraiyani03@users.noreply.github.com>
2023-04-21 23:49:53 +0530

    Update Practical_06.java

--- Practical_06.java
+++ Practical_06.java
@@ -5,15 +5,17 @@ public class Practical_06
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the First Number");
-        double a = sc.nextDouble();
-        System.out.println("Enter the second Number");
-        double b = sc.nextDouble();
+
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+
+        // System.out.println("Enter the First Number");
+        // System.out.println("Enter the second Number");
+        // double b = sc.nextDouble();
+        
         double c;
         c = a*b;
         System.out.println(c);
-
-
 
     }
 }
